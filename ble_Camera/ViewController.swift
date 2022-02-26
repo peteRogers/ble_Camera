4addde889fd0ae99feee504df8b634f8162f4894
Peter Rogers <p.rogers@gold.ac.uk>
2022-02-26 09:49:31 +0000

    Initial Commit

---
+++ ble_Camera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ble_Camera
+//
+//  Created by Peter Rogers on 26/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
